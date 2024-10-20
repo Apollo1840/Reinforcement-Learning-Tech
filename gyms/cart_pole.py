c71f1238ebd94856937433e4fe4f20a401669996
mbk <zoucongyu1993@hotmail.com>
2024-10-20 19:05:22 +0200

    original render

--- gyms/cart_pole.py
+++ gyms/cart_pole.py
@@ -1,18 +1,20 @@
-import gym
 import matplotlib.pyplot as plt
 from IPython.display import display, clear_output
+from gym.envs.classic_control.cartpole import CartPoleEnv
 
 
-class CartPoleV1():
+class CartPoleEnvJP(CartPoleEnv):
 
     def __init__(self):
-        self.env = gym.make('CartPole-v1', render_mode='rgb_array')
+        super().__init__()
+        self.viewer = None  # Initialize the viewer as None
+        self.render_mode = 'rgb_array'  # Set the render mode to 'human'
 
-    def render(self, step_number):
-        img = self.env.render()  # Render and get the current frame as an image
+    def render(self, **kwargs):
+        img = super().render()  # Render and get the current frame as an image
         plt.imshow(img)
         plt.axis('off')
-        plt.title(f"Step: {step_number}")  # Add the step number as a title to the plot
-        
-         display(plt.gcf())
+        plt.title(f"Step: {kwargs['step_number']}")  # Add the step number as a title to the plot
+
+        display(plt.gcf())
         clear_output(wait=True)
--- gyms/frozen_lake.py
+++ gyms/frozen_lake.py
@@ -1,28 +1,36 @@
-import gym
+from gym.envs.toy_text import FrozenLakeEnv
 import matplotlib.pyplot as plt
 import numpy as np
 from matplotlib import colors, patches
 from PIL import Image
 from IPython.display import display, clear_output
 import time
+import os
 
-class FrozenLakeV1():
+
+class FrozenLakeEnvJP(FrozenLakeEnv):
+    # Construct the path to the image files
+    base_path = os.path.dirname(__file__)
+    path_player_img = os.path.join(base_path, "material/frozen_lake/player.jpg")
+    path_hole_img = os.path.join(base_path, "material/frozen_lake/hole.jpg")
+    path_entrance_img = os.path.join(base_path, "material/frozen_lake/entrance.jpg")
+    path_goal_img = os.path.join(base_path, "material/frozen_lake/goal.jpg")
 
     def __init__(self):
-        self.env = gym.make('FrozenLake-v1', is_slippery=True)
+        super().__init__()
 
         # Load the custom images
-        self.player_img = Image.open("material/frozen_lake/player.jpg").resize((40, 40))
-        self.hole_img = Image.open("material/frozen_lake/hole.jpg").resize((40, 40))
-        self.entrance_img = Image.open("material/frozen_lake/entrance.jpg").resize((40, 40))
-        self.goal_img = Image.open("material/frozen_lake/goal.jpg").resize((40, 40))
+        self.player_img = Image.open(self.__class__.path_player_img).resize((40, 40))
+        self.hole_img = Image.open(self.__class__.path_hole_img).resize((40, 40))
+        self.entrance_img = Image.open(self.__class__.path_entrance_img).resize((40, 40))
+        self.goal_img = Image.open(self.__class__.path_goal_img).resize((40, 40))
 
-    def render(self, step_number):
+    def render(self, **kwargs):
         # Get the grid layout (env.desc stores the grid description)
-        grid_array = np.array(self.env.desc, dtype='str')
+        grid_array = np.array(self.desc, dtype='str')
 
         # Get the agent's position (env.s gives the current state as a flat index)
-        player_position = self.env.s
+        player_position = self.s
 
         # Convert the flat position to 2D coordinates (row, col)
         grid_size = grid_array.shape[0]
@@ -60,7 +68,8 @@ class FrozenLakeV1():
         player_ax.set_yticks([])  # Hide y-axis ticks
 
         # Set the step number as the title
-        fig.suptitle(f"Step: {step_number}", y=1.05, fontsize=16)
+        fig.suptitle(f"Episode: {kwargs.get('episode_number', None)}, Step: {kwargs.get('step_number', None)}", y=1.05,
+                     fontsize=16)
 
         # Show the plot
         # plt.show()
